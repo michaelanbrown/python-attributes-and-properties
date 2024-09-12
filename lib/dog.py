5bb94a96a9ff03f5e47b50928cc7521935da867d
michaelanbrown <m_brownbrown@yahoo.com>
2024-09-12 17:03:35 -0400

    added the else statement

--- lib/dog.py
+++ lib/dog.py
@@ -30,3 +30,5 @@ class Dog:
     def set_breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
